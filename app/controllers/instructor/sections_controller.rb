807ee26dc4da44ca7a82c80ec40fec62c4c17077
Aleks Gorbenko <kucher12@gmail.com>
2016-06-02 17:29:47 +0000

    added lessons page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -20,11 +20,12 @@ class Instructor::SectionsController < ApplicationController
   def require_authorized_for_current_course
     if current_course.user != current_user
       return render text: 'Unauthorized', status: :unauthorized
+    end
   end
 
   helper_method :current_course
   def current_course
-    @current_course = Course.find(params[:course_id])
+    @current_course ||= Course.find(params[:course_id])
   end
 
   def section_params
