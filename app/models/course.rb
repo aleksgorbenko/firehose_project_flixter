ee0506ce5110c1668209ceb366f478fd38c1e742
Aleks Gorbenko <kucher12@gmail.com>
2016-06-07 21:14:53 +0000

    added stripe payments to premium courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -7,4 +7,12 @@ class Course < ActiveRecord::Base
   validates :title, presence: true, length: { within: 5..40 }
   validates :description, presence: true, length: { within: 30..500 }
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
+
+  def free?
+    cost.zero?
+  end
+
+  def premium?
+    !free?
+  end
 end
